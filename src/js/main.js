92365dffa71163828512150043a1f85b125efaef
Thales <mbthales10@gmail.com>
2021-01-12 19:44:42 -0300

    Aplication improvement

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,21 @@
-const menu = document.querySelector("[data-js=menu]");
-const hamburguerMenu = document.querySelector("[data-js=hamburguer-menu]");
+const hamburgerBtn = document.querySelector("[data-js=hamburger-btn]");
+const closeMobileMenuBtn = document.querySelector("[data-js=close-mobile-menu-btn");
+const mobileMenu = document.querySelector("[data-js=mobile-menu]");
 
-const activeMenuMobile = () => {
-  menu.classList.add("menu-mobile");
-  menu.classList.remove("menu-desktop");
+const activateMenuMobile = () => {
+  mobileMenu.classList.add("mobile-menu-active");
+  mobileMenu.classList.remove("mobile-menu-disabled");
 }
 
-hamburguerMenu.addEventListener("click", () => {
-  activeMenuMobile();
+const disableMenuMobile = () => {
+  mobileMenu.classList.add("mobile-menu-disabled");
+  mobileMenu.classList.remove("mobile-menu-active");
+}
+
+hamburgerBtn.addEventListener("click", () => {
+  activateMenuMobile();
 });
+
+closeMobileMenuBtn.addEventListener("click", () => {
+  disableMenuMobile();
+})
